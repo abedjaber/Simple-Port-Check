a2339a160718d36a82f15002eba88aed01f61b79
Abed Jaber <abed.jaber81@gmail.com>
2025-04-14 08:23:25 +0300

    Add project files.

---
+++ TestConsole/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Net;using System.Net.Sockets;
+using zkemkeeper;
+
+namespace TestConsole
+    {
+    internal class Program
+        {
+
+
+        static void Main(string[] args)
+            {
+            Console.WriteLine("Enter Destinatin IP Address: ");
+            var ip = Console.ReadLine();
+            Console.WriteLine("Enter Destinatin Port ");
+            var input = Console.ReadLine();
+            int port = Int32.Parse(input);
+            using (TcpClient tcpClient = new TcpClient())
+                {
+                try
+                    {
+                    tcpClient.Connect(ip, port);
+                    Console.WriteLine("Port " + "'" + port + "'" + " on " + "'" + ip + "'" + " is Open!");
+                    }
+                catch (Exception)
+                    {
+                    Console.WriteLine("Port " + "'" + port + "'" + " on " + "'" + ip + "'" + " is Closed!");
+                    }
+                }
+            }
+        }
+    }
+
+
+
