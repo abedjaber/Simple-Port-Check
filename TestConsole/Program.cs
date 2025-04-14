a182e08e51770834b2df9bcca375ca1b79df3bb5
Abed Jaber <abed.jaber81@gmail.com>
2025-04-14 09:04:26 +0300

    12

--- TestConsole/Program.cs
+++ TestConsole/Program.cs
@@ -1,18 +1,19 @@
 ﻿using System;
-using System.Net;using System.Net.Sockets;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
 using zkemkeeper;
-
 namespace TestConsole
     {
     internal class Program
         {
-
-
         static void Main(string[] args)
             {
-            Console.WriteLine("Enter Destinatin IP Address: ");
+        //Label
+        StartCode:
+            Console.WriteLine("\nEnter Destinatin IP Address: ");
             var ip = Console.ReadLine();
-            Console.WriteLine("Enter Destinatin Port ");
+            Console.WriteLine("\nEnter Destinatin Port ");
             var input = Console.ReadLine();
             int port = Int32.Parse(input);
             using (TcpClient tcpClient = new TcpClient())
@@ -20,16 +21,33 @@ namespace TestConsole
                 try
                     {
                     tcpClient.Connect(ip, port);
-                    Console.WriteLine("Port " + "'" + port + "'" + " on " + "'" + ip + "'" + " is Open!");
+                    Console.WriteLine("\nPort " + "'" + port + "'" + " on " + "'" + ip + "'" + " is Open!");
+                    Console.WriteLine("\nTry Again? 1 = Yes, 0 = No ");
+                    var continue1 = Console.ReadLine();
+                    if(continue1 == "1")
+                        {
+                        goto StartCode;
+                        }
+                    else
+                        {
+                        Environment.Exit(0);
+                        }
                     }
                 catch (Exception)
                     {
-                    Console.WriteLine("Port " + "'" + port + "'" + " on " + "'" + ip + "'" + " is Closed!");
+                    Console.WriteLine("\nPort " + "'" + port + "'" + " on " + "'" + ip + "'" + " is Closed!");
+                    Console.WriteLine("\nTry Again? 1 = Yes, 0 = No ");
+                    var continue1 = Console.ReadLine();
+                    if (continue1 == "1")
+                        {
+                        goto StartCode;
+                        }
+                    else
+                        {
+                        Environment.Exit(0);
+                        }
                     }
                 }
             }
         }
     }
-
-
-
